b143e330bc17cdeccf5109e8a94de8d09c165af2
junior <junior@beetech.global>
2018-01-16 09:15:36 -0200

    feat(eslint): config eslint and editorconfig

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import { shallow } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('App component', () => {
+
+  test('should render', () => {
+    const app = shallow(<App />)
+    expect(app).toBeTruthy()
+  })
+})
