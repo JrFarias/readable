a68fe2913cfa493c1cbfd5a08d9dfbc7648338e2
JuniorFarias <junior@beetech.global>
2018-02-05 11:26:09 -0200

    feat(redux): add redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './pages';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './pages'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './ducks'
 import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+<Provider store={store}>
+  <App />
+</Provider>, document.getElementById('root'));
+
