91bb5aafbd1e8567ffc6506bdd9056472f7c9876
junior Farias <junior@beetech.global>
2018-02-13 23:03:27 -0200

    feat(API): add api server

--- src/atoms/Vote/Vote.test.js
+++ src/atoms/Vote/Vote.test.js
@@ -34,33 +34,6 @@ describe('Vote component', () => {
     expect(wrapper.find('.Vote__negative').simulate('click').length).toEqual(1)
   })
 
-  test('should render with voteScore negative', () => {
-    const props = {
-      voteScore: -5,
-      postId: '123',
-      actions: {
-        upVote: () => {},
-        downVote: () => {}
-      }
-    }
-    const defaultVoteScore = '0';
-
-    const wrapper = mount(
-      <Vote
-        voteScore={props.voteScore}
-        postId={props.postId}
-        actions={props.actions}
-      />
-    )
-
-    expect(wrapper).toBeTruthy()
-    expect(wrapper.props().voteScore).toEqual(props.voteScore)
-    expect(wrapper.props().postId).toEqual(props.postId)
-    expect(wrapper.find('.Vote__positive').text()).toEqual(defaultVoteScore)
-    expect(wrapper.find('.Vote__negative').text()).toEqual(`${props.voteScore}`)
-
-  })
-
   test('should render with voteScore positive', () => {
     const props = {
       voteScore: 5,
